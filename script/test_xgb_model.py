c67c4321874e90ed205e73b68310d676c1770a55
raulcamp <raulcamp@mit.edu>
2022-06-11 11:33:20 -0700

    updated xgboost test file + test dataset

--- script/test_xgb_model.py
+++ script/test_xgb_model.py
@@ -1,25 +1,18 @@
 import pandas as pd
 import pickle
 from sklearn.metrics import mean_squared_error, accuracy_score, f1_score, precision_score, recall_score
-from train_xgb_model import feature_transform
-
-def unfeaturize(data):
-    del data['numSD']
-    del data['hasPercent']
-    del data['special']
-    del data['numerical']
-    del data['len_url']
+from url_featurizer import URLFeaturizer
     
+def transform(df):
+    rows = [URLFeaturizer(URL).extract() for URL in df['URL']]
+    return pd.concat([pd.DataFrame(rows), df.drop(columns=['URL'])], axis=1, join='inner')
+
 def test(input_model, input_test_data_path, full_test_data_path): 
     # load model
     model = pickle.load(open(input_model, 'rb'))
     # load and transform test data
-    test_data = pd.read_csv(input_test_data_path)
-    ids = test_data['Unnamed: 0']
-    urls = test_data['URL']
-    del test_data['Unnamed: 0']
-    del test_data['Label']
-    feature_transform(test_data)
+    raw_data = pd.read_csv(input_test_data_path)
+    test_data = transform(raw_data.drop(columns=['index', 'Label']))
     # run model on test data
     predictions = model.predict(test_data)
     # validate predicted labels
@@ -30,18 +23,12 @@ def test(input_model, input_test_data_path, full_test_data_path):
             precision_score(actual, predictions),
             recall_score(actual, predictions),
             f1_score(actual, predictions))
-    
-    # unfeaturize(test_data)
-    # test_data.insert(0, '',ids )
-    # test_data.insert(4, 'URL', urls)
-    # test_data.insert(5, 'Label', labels)
-    # test_data.to_csv(r'script/data/Phishing_Labels.csv', index=False)
 
 if __name__ == '__main__':
     model_path = 'script/models/xgb_model'
     input_test_data_path = 'script/data/Phishing_Test.csv'
     full_test_data_path = 'script/data/Phishing_Test_Full.csv'
-    # test
+    # test model
     rmse, accuracy, precision, recall, f1 = test(model_path, input_test_data_path, full_test_data_path)
     print("RMSE: %f" % (rmse))
     print("f1: %f" % (f1))
